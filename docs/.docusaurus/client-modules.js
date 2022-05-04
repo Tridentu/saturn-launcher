4f50946c7d48d6668ff2c1b8798e25a3ba47f2c3
Aerodos12 <weldon.henson97@gmail.com>
2022-05-04 15:08:35 -0400

    Added Docs

---
+++ docs/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/sources/projects/io.github.tridentu.saturn/docs/node_modules/infima/dist/css/default/default.css'),
+  require('/sources/projects/io.github.tridentu.saturn/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/sources/projects/io.github.tridentu.saturn/docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/sources/projects/io.github.tridentu.saturn/docs/src/css/custom.css'),
+];
